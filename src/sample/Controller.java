7ed46954bb2c381469f714c414a6bfb68383f4b6
trantoai1 <52073804+trantoai1@users.noreply.github.com>
2020-12-28 16:31:31 +0700

    finish bai17

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,10 @@
 package sample;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class Controller {
-
+    /*Bài 1: Viết chương trình chuyển đổi một số tự nhiên ở hệ cơ số 10 thành số ở hệ cơ số b bất kì (1< b≤ 24). */
     public static String doiHeSo(int coso, int number)
     {
         String []A = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N"};
@@ -18,28 +17,31 @@ public class Controller {
         }
         return "result:"+result;
     }
-    /*Bài 2*/
-    public static String tongCacChuSo(Long number)
+    /*Bài 2:Hãy viết chương trình tính tổng các chữ số của một số nguyên bất kỳ.*/
+    private  static  int sumChuSo(int number) throws  NumberFormatException
     {
-        //Long tem = new Long("999999999999");
-
-        int sum = -1;
-        try {
-            sum = 0;
+        int sum = 0;
             while(number>0)
             {
                 sum+= number%10;
                 number/=10;
             }
-            return "result:"+String.valueOf(sum);
+            return sum;
+
+    }
+    public static String tongCacChuSo(int number)
+    {
+        try {
+
+            return "result:"+String.valueOf(sumChuSo(number));
         } catch (Exception e) {
             return "error:"+e.getMessage();
         }
     }
-    /*Bài 3*/
+    /*Bài 3:Viết chương trình phân tích một số nguyên thành các thừa số nguyên tố */
     public static String thuaSoNguyenTo(int number)
     {
-        String result = "";
+        String result = "result:";
         int i=2;
         while(i <=number)
         {
@@ -52,12 +54,13 @@ public class Controller {
 
         }
         result=result.substring(0, result.length()-1);
-        return "result:"+result;
+        return result;
     }
-    /*Bài 4*/
-    public static boolean isSNT(int number)
+    /*Bài 4:Viết chương trình liệt kê tất cả các số nguyên tố nhỏ hơn n cho trước*/
+    private static boolean isSNT(int number)
     {
         int count = 0;
+        if(number <=1) return false;
         for (int i = 2; i < number; i++) {
             if (number%i==0) count++;
         }
@@ -67,14 +70,14 @@ public class Controller {
     }
     public static String cacSoNguyenTo(int number)
     {
-        String result = "";
+        String result = "result:";
         for (int i = 2; i < number; i++) {
             if(isSNT(i)) result+= i+"|";
         }
         result=result.substring(0, result.length()-1);
-        return "result:"+result;
+        return result;
     }
-    /*Bài 5*/
+    /*Bài 5:Một số được gọi là số thuận nghịch độc nếu ta đọc từ trái sang phải hay từ phải sang trái số đó ta vẫn nhận được một số giống nhau. Hãy liệt kê tất cả các số thuận nghịch độc có sáu chữ số */
     public static boolean isThuanNghich(int number)
     {
         String num = String.valueOf(number);
@@ -84,17 +87,16 @@ public class Controller {
     }
     public static String lietKeThuanNghich()
     {
-        String result = "";
+        String result = "result:";
         for (int i = 100000; i < 1000000; i++) {
             if(isThuanNghich(i))result+= i+"|";
         }
         result=result.substring(0, result.length()-1);
-        return "result:"+result;
+        return result;
     }
-    /*Bài 6,7*/
-    public static String dayXuatHien(List<Integer> ds, int solan)
+    /*Bài 6,7,8: Hãy liệt kê các phần tử xuất hiện trong dãy*/
+    private static   Map<Integer,Integer> lietKeDay(List<Integer> ds)
     {
-        String result = "";
         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
         for (Integer d : ds) {
             if(map.containsKey(d))
@@ -103,33 +105,132 @@ public class Controller {
                 map.put(d, 1);
 
         }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+       // Collections.sort(ds,(o1, o2) -> o1.compareTo(o2)>0?-1:1);
+
+        return  map;
+    }
+    public static String dayXuatHien(List<Integer> ds, int solan)
+    {
+        String result = "result:";
+
+        for (Map.Entry<Integer, Integer> entry : lietKeDay(ds).entrySet()) {
             Integer key = entry.getKey();
             Integer value = entry.getValue();
             if(value == solan)
                 result+= key+"|";
         }
         result=result.substring(0, result.length()-1);
-        return "result:"+result;
+        return result;
     }
     public static String lietKeSoLan(List<Integer> ds)
     {
-        String result = "";
-        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
-        for (Integer d : ds) {
-            if(map.containsKey(d))
-                map.put(d, map.get(d)+1);
-            else
-                map.put(d, 1);
-
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        String result = "result:";
+        for (Map.Entry<Integer, Integer> entry : lietKeDay(ds).entrySet()) {
             Integer key = entry.getKey();
             Integer value = entry.getValue();
 
-                result+= key+"|";
+                result+= key+"&"+value+"|";
         }
         result=result.substring(0, result.length()-1);
-        return "result:"+result;
+        return result;
+    }
+    /*Bài 10: Viết chương trình liệt kê tất cả các số nguyên tố có 5 chữ số sao cho tổng của các chữ số trong mỗi số nguyên tố đều bằng S cho trước*/
+    public static String lietKeSNT(int S)
+    {
+        String result = "result:";
+        for (int i=10000;i<=99999;i++)
+        {
+            if(isSNT(i)&&sumChuSo(i)==S)
+            {
+                result+= i+"|";
+            }
+        }
+        result=result.substring(0, result.length()-1);
+        return result;
+    }
+    /*Bài 11: Nhập một số tự nhiên n. Hãy liệt kê các số Fibonaci nhỏ hơn n là số nguyên tố. */
+    public static String fibo_lt(int n)
+    {
+        String result = "result:1|";
+        int fi=1,i=1;
+
+       while(fi<n)
+       {
+           result+=fi+"|";
+           int temp = fi;
+            fi+=i;
+            i= temp;
+
+       }
+        result=result.substring(0, result.length()-1);
+        return result;
+    }
+    /*Bài 15: Viết chương trình nhập vào vào ma trận A có n dòng, m cột, các phần tử là những số nguyên lớn hơn 0 và nhỏ hơn 100 được nhập vào từ bàn phím*/
+    public static int maxMatrix(int [][]matrix,int n,int m)
+    {
+        int []maxs = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            maxs[i] = Arrays.stream(matrix[i]).max().getAsInt();
+        }
+        int max = Arrays.stream(maxs).max().getAsInt();
+        return max;
+    }
+    public  static  String timNguyenTo(int [][]matrix,int n,int m)
+    {
+        String result = "result:";
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<m;j++)
+            {
+                if(isSNT(matrix[i][j]))
+                {
+                    result+= matrix[i][j]+"|";
+                }
+                else
+                {
+                    matrix[i][j] = 0;
+                    result+= matrix[i][j]+"|";
+                }
+            }
+        }
+        result=result.substring(0, result.length()-1);
+        return result;
+    }
+    public static void sortColumes(int [][]matrix,int n,int m)
+    {
+        int []temp = new int[n];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                temp[j]=matrix[j][i];
+            }
+            IntStream stream = Arrays.stream(temp).sorted();
+            temp = stream.toArray();
+            for(int j=0;j<n;j++)
+            {
+                matrix[j][i] = temp[j];
+            }
+        }
+
+    }
+    /*Bài 16:Viết chương trình nhập vào vào mảng A có n phần tử*/
+    public static void sortMatrix(int[] matrix,int n)
+    {
+        Integer[] what = Arrays.stream( matrix ).boxed().toArray( Integer[]::new );
+        //IntStream stream=Arrays.stream(matrix).sorted();
+
+        List<Integer> list = Arrays.asList(what);
+        list.sort(((o1, o2) -> o1 > o2?-1:1));
+        int k=0;
+        for (Integer i: list
+             ) {
+
+            matrix[k] = i;
+            k++;
+        }
+
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,20 +6,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+public class Main {
+
 
 
     public static void main(String[] args) {
-
+        //System.out.println("Bài 10:");
         //launch(args);
+       // System.out.println(Controller.lietKeSNT(13));
+        //System.out.println("Bài 11:");
+       // System.out.println(Controller.fibo_lt(12));
+        //Integer[] n ={9,2,67,3,4,6};
+       // Optional max =Arrays.stream(n).max((o1, o2) -> o1>o2?1:-1);
+        //System.out.println(max);
+        int [][]matrix ={{1,2,3,6},{2,84,3,2},{7,8,192,5}};
+        //System.out.println(Controller.maxMatrix(matrix,3,3));
 
+       // System.out.println(Controller.timNguyenTo(matrix,3,3));
+        Controller.sortColumes(matrix,3,4);
+        for (int i=0;i<3;i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
     }
 }
